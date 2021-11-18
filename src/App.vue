742814526865bcc3bea1eecb1bf97e62170c0d02
sciug <gaetano.sciurti@libero.it>
2021-11-18 11:26:07 +0100

    show movies

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" v-model="querySearch" placeholder="search a movie">
+    <button @click="searchMovie">search</button>
+    <div v-for="movie in movies" :key="movie.id">
+      <div class="movie">
+      <p>title: {{movie.title}}</p>
+      <p>original title: {{movie.original_title}}</p>
+      <p>original language: {{movie.original_language}}</p>
+      <p>vote: {{movie.vote_average}}</p>
+
+      </div>
+  
+
+
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  data(){
+    return {
+      movies:[],
+      querySearch:"",
+      error: ""
+    }
   },
+  methods:{
+    callApi(){
+      axios
+      .get("https://api.themoviedb.org/3/search/movie?api_key=31604f6ab3ca5fcc65adf409f092f7c1&language=en-US&query="+this.querySearch+"&page=1&include_adult=false")
+      .then((response)=> {
+        console.log(response.data.results)
+        this.movies = response.data.results
+      })
+      .catch((e) => {
+          console.log(e, "oops error");
+          this.error = e;
+        });
+    },
+    searchMovie(){
+      this.callApi()
+      console.log(this.Api_url)
+    }
+
+    
+  }
+
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.movie{
+  border: 1px solid red;
+  margin:1rem 0rem;
 }
 </style>
