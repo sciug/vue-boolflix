c9d950f6848b45bc361fe2676ec03d20cf67d065
sciug <gaetano.sciurti@libero.it>
2021-11-18 16:54:20 +0100

    fix multiple concurrent issue add stars

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <div class="movie">
         <img :src='"https://image.tmdb.org/t/p/"+"w342"+ movie.poster_path'  alt="" v-if="movie.poster_path !== null">
         <img :src='"https://image.tmdb.org/t/p/"+"w342"+ movie.backdrop_path'  alt="" v-else-if="movie.poster_path === null && movie.backdrop_path !==null">
-        <img src="http://www.cfpcemon.it/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png" width="342px" alt="" v-else>
+        <img src="http://www.cfpcemon.it/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png" class="not_found_img"  alt="" v-else>
       <p>title: {{movie.title}}</p>
       <p>original title: {{movie.original_title}}</p>
       <div class="language">
@@ -16,7 +16,10 @@
 
 
       </div>
-      <p>vote: {{movie.vote_average}}</p>
+      <p>vote: {{transformNumber(movie.vote_average)}}</p>
+      <div v-for="i in transformNumber(movie.vote_average)" :key="i">
+        <i class="fas fa-star"></i>
+      </div>
       <p><i class="fas fa-adjust"></i></p>
 
       </div>
@@ -39,8 +42,10 @@
 
 
       </div>
-      <p>vote: {{serie.vote_average}}</p>
-      <p><i class="fas fa-adjust"></i></p>
+      <p>vote: {{transformNumber(serie.vote_average)}}</p>
+      <div v-for="i in transformNumber(serie.vote_average)" :key="i">
+        <i class="fas fa-star"></i>
+      </div>
 
       </div>
 
@@ -60,6 +65,7 @@ export default {
       error: "",
       
       
+      
     }
   },
   methods:{
@@ -71,21 +77,26 @@ export default {
     },
 
     searchElement(){
-      Promise.all([this.callMovieApi(), this.callSeriesApi])
-      .then(function (results){
-        this.movies = results[0];
-        this.series = results[1]
-      })
+       Promise.all([this.callMovieApi(), this.callSeriesApi()])
+      .then(axios.spread((...results)=>{
+        this.movies = results[0].data.results
+        this.series = results[1].data.results
+      }))
       .catch((e) => {
         console.log(this.movies)
          this.error = e;
           console.log(e, "oops error");
          
         });
+    },
+    transformNumber(number){
+      return Math.floor(number / 2)
     }
 
+
     
-  }
+  },
+  
 
 };
 </script>
@@ -98,5 +109,11 @@ export default {
 .serie{
     border: 1px solid blue;
   margin:1rem 0rem;
+}
+.not_found_img{
+  height:513px;
+  width:342px;
+  object-fit:cover;
+
 }
 </style>
